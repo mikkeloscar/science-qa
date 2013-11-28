cc52bd7ba1bb0c1085f821dfc6a82d6924ec9283
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2013-11-28 16:41:43 +0100

    Add new logout page

--- science_qa/urls.py
+++ science_qa/urls.py
@@ -21,5 +21,6 @@ urlpatterns = patterns('',
     url(r'^apikey/', include('api_key_manager.urls')),
     url(r'^login/', 'django.contrib.auth.views.login', {
         'template_name': 'login.html' }),
-    url(r'^logout/', 'django.contrib.auth.views.logout'),
+    url(r'^logout/', 'django.contrib.auth.views.logout', {
+        'template_name': 'logout.html' }),
 )
